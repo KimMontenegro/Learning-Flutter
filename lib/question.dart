234b6a821dbf7e739ebb699937a432b72049bdc6
Kimberly Mae Montenegro <mkimberlymae0812@gmail.com>
2020-07-24 04:28:52 +0800

    Added style and width

--- lib/question.dart
+++ lib/question.dart
@@ -3,13 +3,24 @@ import 'package:flutter/material.dart';
 ///just stype "st" then choose StatelessWidget to automatically
 ///have certain code
 class Question extends StatelessWidget {
-  String questionText;
+  final String questionText;
 
   ///first argument passed to question constructor
   ///will be stored in the questionText property
   Question(this.questionText);
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    //wrap another widget(Text) called Container
+    //Container takes a child name argument
+    return Container(
+
+        ///infinity - will set and adjust based on the
+        ///full width of the device
+        width: double.infinity,
+        child: Text(
+          questionText,
+          style: TextStyle(fontSize: 28),
+          textAlign: TextAlign.center,
+        ));
   }
 }
