74253197aa1066d7fec850361d36b6b2ba409316
Kimberly Mae Montenegro <mkimberlymae0812@gmail.com>
2020-07-23 19:22:47 +0800

    Much cleaner and nicer output

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart'; //unlocks a lot of feature
 
-void main() {
-  runApp(MyApp()); //normal function written by flutter team
-}
+// void main() {
+//   runApp(MyApp()); //normal function written by flutter team
+// }
+void main() => runApp(MyApp()); //if only 1 line of code
 
 /*
 extends - class will be based on some other class
@@ -15,14 +16,21 @@ class MyApp extends StatelessWidget {
   build-method that returns a Widget
   MaterialApp-special widget that should be returned
 */
+  @override //decorator-make the code clearer and cleaner
   Widget build(BuildContext context) {
     /*home: - argument; the core widget w/c flutter will bring
     on the screen
     MaterialApp is a class and yet we can pass data to it
     with a feature called constructor
+    Scaffold()-creating a basic page design like colors
     */
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text('This is my default text!'),
+      ),
     );
   }
 }
