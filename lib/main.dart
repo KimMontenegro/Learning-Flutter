d66e3c1f67c396b5d5d3aeec97a19eee7d9fbb57
Kimberly Mae Montenegro <mkimberlymae0812@gmail.com>
2020-07-23 21:16:15 +0800

    Button is pressed

--- lib/main.dart
+++ lib/main.dart
@@ -5,25 +5,27 @@ import 'package:flutter/material.dart'; //unlocks a lot of feature
 // }
 void main() => runApp(MyApp()); //if only 1 line of code
 
-/*
-extends - class will be based on some other class
-StatelessWidget - baseclass that allows to create own widget
-extends StatelessWidget - turn the whole class that will be used 
-as widget in Flutter
-*/
+///extends - class will be based on some other class
+///StatelessWidget - baseclass that allows to create own widget
+///extends StatelessWidget - turn the whole class that will be used
+///as widget in Flutter
+
 class MyApp extends StatelessWidget {
-/*BuildContext - annotation provided by flutter
-  build-method that returns a Widget
-  MaterialApp-special widget that should be returned
-*/
+  void answerQuestion() {
+    print('Answer Chosen!');
+  }
+
+  ///BuildContext - annotation provided by flutter
+  ///build-method that returns a Widget
+  //MaterialApp-special widget that should be returned
   @override //decorator-make the code clearer and cleaner
   Widget build(BuildContext context) {
-    /*home: - argument; the core widget w/c flutter will bring
-    on the screen
-    MaterialApp is a class and yet we can pass data to it
-    with a feature called constructor
-    Scaffold()-creating a basic page design like colors
-    */
+    ///home: - argument; the core widget w/c flutter will bring
+    ///on the screen
+    ///MaterialApp is a class and yet we can pass data to it
+    ///with a feature called constructor
+    ///Scaffold()-creating a basic page design like colors
+
     var questions = [
       'What\'s your favorite color?',
       'What\'s your favorite animal?',
@@ -38,15 +40,15 @@ class MyApp extends StatelessWidget {
             Text('The question!'),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: null,
+              onPressed: answerQuestion, //execute when user presses the button
             ),
             RaisedButton(
               child: Text('Answer 2'),
-              onPressed: null,
+              onPressed: answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 3'),
-              onPressed: null,
+              onPressed: answerQuestion,
             ),
           ],
         ),
