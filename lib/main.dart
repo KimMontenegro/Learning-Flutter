2000a5ba6ddaccef771a9dc3ef71b45115c790ae
Kimberly Mae Montenegro <mkimberlymae0812@gmail.com>
2020-07-24 19:29:18 +0800

    Getters & else-if

--- lib/main.dart
+++ lib/main.dart
@@ -32,22 +32,39 @@ class _MyAppState extends State<MyApp> {
   final _questions = const [
     {
       'questionText': 'What\'s your favorite color?',
-      'answers': ['Black', 'Red', 'Green', 'White'],
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ],
     },
     {
       'questionText': 'What\'s your favorite animal?',
-      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 11},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Lion', 'score': 9},
+      ],
     },
     {
       'questionText': 'What\'s your favorite instructor?',
-      'answers': ['Max', 'Max', 'Max', 'Max'],
+      'answers': [
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+      ],
     },
   ];
   var _questionIndex = 0; //to change dynamically when pressing a button
-
-  void _answerQuestion() {
+  var _totalScore = 0;
+  void _answerQuestion(int score) {
     // var aBool = true;
     // aBool =false;
+
+    _totalScore += score;
 
     ///wrap the code when changing data that is reflected in UI
     ///wrap it using setState
--- lib/quiz.dart
+++ lib/quiz.dart
@@ -22,8 +22,9 @@ class Quiz extends StatelessWidget {
           questions[questionIndex]
               ['questionText'], //accessing index number then the question
         ),
-        ...(questions[questionIndex]['answers'] as List<String>).map((answer) {
-          return Answer(answerQuestion, answer);
+        ...(questions[questionIndex]['answers'] as List<Map<String, Object>>)
+            .map((answer) {
+          return Answer(() => answerQuestion, answer['text']);
         }).toList() //convert map to a list
       ],
     );
